451fcab0ddf2fa2dd4885fda0bd55926b727b8e7
Byte <bytehash69@gmail.com>
2026-01-29 15:39:18 +0530

    fix(vault): removed fault vault program for refactoring

--- anchor/vault/programs/vault/src/instructions/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod deposit;
-pub mod withdraw;
-
-pub use deposit::*;
-pub use withdraw::*;
--- anchor/vault/programs/vault/src/state/mod.rs
+++
@@ -1,9 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[account]
-#[derive(InitSpace)]
-pub struct Vault {
-    pub owner: Pubkey,
-    pub balance: u64,
-    pub bump: u8,
-}
